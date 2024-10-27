2169227ba8b06be0195e5cb9bcf5c14fa932ec78
Nguyen Quang Thuan <kiaisoft.thuan.nguyen@gmail.com>
2024-10-27 17:32:51 +0700

    Deploy project to vercel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = 8080; // cấu hình port
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // cho phép truyền json qua body
 
 app.post('/api/v1/iot', (req, res) => {
+    // lấy các giá trị từ body gửi lên
     const { deviceId, qrCodeId, qrCodeValue } = req.body;
 
+    // trả về phản hồi thành công với code là 200 kèm theo các giá trị
     res.status(200).json({
         message: "Get data successful!",
         code: 200,
